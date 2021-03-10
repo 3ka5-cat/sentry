520e31c3017f61e55f250bee284d2b665620ae2c
Taylan Gocmen <ta.gocmen@gmail.com>
2021-03-10 11:41:18 -0800

    convert metric text to get fn

--- src/sentry/static/sentry/app/views/alerts/rules/details/body.tsx
+++ src/sentry/static/sentry/app/views/alerts/rules/details/body.tsx
@@ -85,6 +85,21 @@ class DetailsBody extends React.Component<Props> {
     return `${direction} ${value}`;
   }
 
+  getMetricText(): React.ReactNode {
+    const {rule} = this.props;
+
+    if (!rule) {
+      return '';
+    }
+
+    const {aggregate, timeWindow} = rule;
+
+    return tct(' [metric] over [window]', {
+      metric: aggregate,
+      window: <Duration seconds={timeWindow * 60} />,
+    })
+  }
+
   getInterval() {
     const {
       timePeriod: {start, end},
@@ -324,10 +339,7 @@ class DetailsBody extends React.Component<Props> {
           </ChartPanel>
         </Layout.Main>
         <Layout.Side>
-          <SidebarHeading>
-            <span>{t('Alert Rule')}</span>
-          </SidebarHeading>
-          {this.renderRuleDetails()}
+          <Placeholder height="200px" />
         </Layout.Side>
       </Layout.Body>
     );
@@ -353,7 +365,6 @@ class DetailsBody extends React.Component<Props> {
       environment,
       aggregate,
       projects: projectSlugs,
-      timeWindow,
       triggers,
     } = rule;
 
@@ -393,10 +404,7 @@ class DetailsBody extends React.Component<Props> {
                       <PresetName>
                         {this.metricPreset?.name ?? t('Custom metric')}
                       </PresetName>
-                      {tct(' [metric] over [window]', {
-                        metric: aggregate,
-                        window: <Duration seconds={timeWindow * 60} />,
-                      })}
+                      {this.getMetricText()}
                     </ChartHeader>
                     <EventsRequest
                       api={api}
@@ -461,9 +469,6 @@ class DetailsBody extends React.Component<Props> {
               </Layout.Main>
               <Layout.Side>
                 {this.renderMetricStatus()}
-                <SidebarHeading>
-                  <span>{t('Alert Rule')}</span>
-                </SidebarHeading>
                 {this.renderRuleDetails()}
               </Layout.Side>
             </Layout.Body>
